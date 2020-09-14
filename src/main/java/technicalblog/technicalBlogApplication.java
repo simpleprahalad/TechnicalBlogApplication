12e3a8975a7c6a43851f9a77a0c71328011a6477
simpleprahalad <48152622+simpleprahalad@users.noreply.github.com>
2020-09-15 05:21:51 +0530

    First milestone

---
+++ src/main/java/technicalblog/technicalBlogApplication.java
@@ -0,0 +1,12 @@
+package technicalblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class technicalBlogApplication {
+
+    public static void main(String [] args) {
+        SpringApplication.run(technicalBlogApplication.class, args);
+    }
+}
